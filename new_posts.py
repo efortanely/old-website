da5078da0457f6bb3f44b5d31342229857c9562a
Rosemary Fortanely <rosemary.fortanely@gmail.com>
2020-02-23 21:46:56 -0600

    Blog post of week Feb 17

--- new_posts.py
+++ new_posts.py
@@ -89,9 +89,6 @@ except:
     session.add(new_post)
     session.commit()
 
-session.delete(session.query(BlogPost).filter_by(title="CS373 Spring 2020: Rosemary Fortanely — Week of Jan. 27").one())
-session.commit()
-
 new_post = BlogPost(
     title="CS373 Spring 2020: Rosemary Fortanely — Week of Jan. 27", 
     date="2-2-2020", 
@@ -213,6 +210,91 @@ new_post = BlogPost(
     <br><br>Heroku is a great tool for getting urls and hosting websites. It’s free at a certain tier \
     and very straight forward to use!"
 
+)
+try:
+    session.query(BlogPost).filter_by(title=new_post.title).one()
+except:
+    session.add(new_post)
+    session.commit()
+
+new_post = BlogPost(
+    title="CS373 Spring 2020: Rosemary Fortanely — Week of Feb. 10", 
+    date="2-15-2020", 
+    content="<img src='/static/rosemary.jpeg' align='left' class='border'>\
+    <b>1. What did you do this past week?</b>\
+    <br><br>Over the past week, I reviewed the operator material I missed last Friday and also started up on \
+    research with Connor for one of the project ideas for the upcoming website. We created the scheme for a \
+    few models, including the Pet, Breed, and Shelter model, as well as found a Austin Animal Center dataset \
+    of information over dog and cat intakes at a local shelter and a couple of APIs about cat and dog breeds, \
+    in addition to couple of adoption websites that can be scraped for data to supplement the Austin Animal \
+    Center dog and cat dataset.\
+    <br><br><b>2. What's in your way?</b>\
+    <br><br>Right now the group is mostly just waiting on having our first meeting before we go ahead with \
+    anything. Presumably in this meeting we will go over the results from a poll we set up to decide which \
+    project proposal we liked the most (a few of us proposed the animal adoption website, the Austin music \
+    website, and an exercise sources website), as well as determining what roles each of us want to take \
+    going forward for this first part of the project.\
+    <br><br><b>3. What will you do next week?</b>\
+    <br><br>I am not sure exactly which part of the project I will be working on, but my interests in web \
+    development probably lean more towards back-end development, but I would like to also improve my skills \
+    in front-end development, specifically in React and TypeScript, as well as graphic design.\
+    <br><br><b>4. What was your experience of Project #1: Collatz (the problem, the overkill requirements \
+    of submission, etc.)?</b>\
+    <br><br>I thought Collatz was a fun problem, but I do agree that all of the different tools we had to \
+    use and steps we had to go through to go from finishing the Hackerrank problem to turning in the final \
+    script and tests was kind of overwhelming. I got through it eventually though, and looking back on it, \
+    the hardest part was simply the Hackerrank portion and getting all the tests to pass. I think I just \
+    had a less favorable experience with the software engineering side of things because it felt like a \
+    bit of drudge work.\
+    <br><br><b>5. What made you happy this week?</b>\
+    <br><br>I was having some issues with getting authentication to work with the Spotify API for my \
+    SpotYourFriends side project, but I finally got that fixed so I could deploy it to Heroku and now \
+    it feels like a real thing instead of just a toy website I run from my terminal, which made me feel \
+    really excited!\
+    <br><br><b>6. What's your pick-of-the-week or tip-of-the-week?</b> \
+    <br><br>Heroku is a great tool for getting urls and hosting websites. It’s free at a certain tier \
+    and very straight forward to use!"
+
+)
+try:
+    session.query(BlogPost).filter_by(title=new_post.title).one()
+except:
+    session.add(new_post)
+    session.commit()
+
+new_post = BlogPost(
+    title="CS373 Spring 2020: Rosemary Fortanely — Week of Feb. 17", 
+    date="2-23-2020", 
+    content="<img src='/static/rosemary.jpeg' align='left' class='border'>\
+    <b>1. What did you do this past week?</b>\
+    <br><br>This past week I’ve been doing a lot of graphic design work and front-end development for \
+    our website. I’ve designed the home page, the about page, and the search page, as well as completed \
+    the corresponding code for those, sans the search page. It was also my first time creating vector \
+    graphics. Even though the designs I made were really simple, I enjoyed learning about that since \
+    it’s been a goal of mine for a while to improve on that skill so I can make more engaging websites.\
+    <br><br><b>2. What's in your way?</b>\
+    <br><br>I think the main thing making it hard to work right now is stress. I like having things done \
+    early and it’s been stressing me out not having control over that and waiting for things to fall \
+    into place. There’s also been some miscommunication and people haven’t been making issues besides \
+    me and Connor which makes it hard to tell who’s working on what.\
+    <br><br><b>3. What will you do next week?</b>\
+    <br><br>At this point, I’m not sure what will be my next work item, if any. I suppose this will be \
+    solidified at our meeting we’re having this Monday.\
+    <br><br><b>4. What was your experience of iteration, AWS, and Chef Secure?</b>\
+    <br><br>I thought some of the tricks we went over during the iteration lecture were neat, such as \
+    how an iterator of an iterator will always return an iterator. I was surprised at how much effort \
+    goes into hosting since before I had only ever used tools like Heroku and GitHub Pages which made \
+    hosting straightforward, so I’m hoping our guys working on hosting can get through that okay. The \
+    mini security lecture from the CEO of Chef Secure has given me a new perspective on \
+    vulnerabilities online.\
+    <br><br><b>5. What made you happy this week?</b>\
+    Today, my boyfriend, Connor, and I went to Michi Ramen off Lamar and I got some yummy ramen and \
+    fries. Even though we had just been there last week for Valentine’s Day, I still enjoyed it \
+    because they have top tier tofu.\
+    <br><br><b>6. What's your pick-of-the-week or tip-of-the-week?</b> \
+    <br><br>Marvel App is a nice tool for creating mock-ups that I’ve used for this and a couple of \
+    previous web design projects, and I’ve found it helpful to have something a little more concrete \
+    than just making a rough sketch on paper."
 )
 try:
     session.query(BlogPost).filter_by(title=new_post.title).one()
